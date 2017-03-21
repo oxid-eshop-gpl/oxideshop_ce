ad33e199e37ee35689541eec1320f8f9672b44ad
hkreuter <heike.reuter@oxid-esales.com>
2017-03-21 09:19:45 +0100

    Demo test modules

---
+++ source/modules/Vendor4/ModuleChainExtension37d/metadata.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of OXID eShop Community Edition.
+ *
+ * OXID eShop Community Edition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eShop Community Edition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2017
+ * @version   OXID eShop CE
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.0';
+
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'          => 'module_chain_extension_3_7d',
+    'title'       => 'Patching \OxidEsales\EshopCommunity\Application\Model\Article directly is forbidden!',
+    'description' => 'The test has three test modules - they are sorted in every possible order in different test cases.',
+    'thumbnail'   => 'picture.png',
+    'version'     => '1.0',
+    'author'      => 'OXID eSales AG',
+    'extend'      => [
+        \OxidEsales\EshopCommunity\Application\Model\Article::class => \OxidEsales\EshopCommunityTestModule\Vendor4\ModuleChainExtension37d\MyClass37d::class]
+);
