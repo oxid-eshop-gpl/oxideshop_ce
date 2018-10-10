16dc54b9bfed412ee43e4521e580077adcb4ece9
Jędrzej Skoczek <j.skoczek@createit.pl>
2018-10-10 17:22:05 +0200

    OXDEV-1376 Convert hasrights tag
    Adding new tag to twig proved to be complex.

---
+++ source/Internal/Twig/Extensions/HasRightsExtension/HasRightsExtension.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\EshopCommunity\Internal\Twig\Extensions\HasRightsExtension;
+
+use Twig\Extension\AbstractExtension;
+
+/**
+ * Class HasRightsExtension
+ *
+ * @package OxidEsales\EshopCommunity\Internal\Twig\Extensions\HasRightsExtension
+ */
+class HasRightsExtension extends AbstractExtension
+{
+
+    /**
+     * @return array|\Twig_TokenParserInterface[]
+     */
+    public function getTokenParsers()
+    {
+        return [new HasRightsParser()];
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'hasrights';
+    }
+
+    /**
+     * @return array|\Twig_NodeVisitorInterface[]
+     */
+    public function getNodeVisitors()
+    {
+        return [new HasRightsVisitor()];
+    }
+}
