3e228623b2c6971fd5c3f5e02f66b305bf7abec0
Tomasz Kowalewski <t.kowalewski@createit.pl>
2018-11-20 14:53:20 +0100

    OXDEV-1376 Convert oxmultilangassign to twig
    Converted smarty oxmultilangassign modifier to multi_lang twig filter

---
+++ tests/Unit/Internal/Twig/Extensions/Filters/MultiLangExtensionTest.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\EshopCommunity\Tests\Unit\Internal\Twig\Extensions\Filters;
+
+use OxidEsales\Eshop\Core\Field;
+use OxidEsales\EshopCommunity\Internal\Adapter\TemplateLogic\MultiLangLogic;
+use OxidEsales\EshopCommunity\Internal\Twig\Extensions\Filters\MultiLangExtension;
+use OxidEsales\EshopCommunity\Tests\Unit\Internal\Twig\Extensions\AbstractExtensionTest;
+
+/**
+ * Class MultiLangExtensionTest
+ *
+ * @author Tomasz Kowalewski (t.kowalewski@createit.pl)
+ */
+class MultiLangExtensionTest extends AbstractExtensionTest
+{
+
+    /** @var MultiLangExtension */
+    protected $extension;
+
+    public function setUp()
+    {
+        $this->extension = new MultiLangExtension(new MultiLangLogic());
+    }
+
+    /**
+     * Provides data to testSimpleTranslating
+     *
+     * @return array
+     */
+    public function simpleTranslatingProvider(): array
+    {
+        return [
+            ["{{ 'FIRST_NAME'|multi_lang }}", 0, 'Vorname'],
+            ["{{ 'FIRST_NAME'|multi_lang }}", 1, 'First name'],
+            ["{{ 'VAT'|multi_lang }}", 1, 'VAT']
+        ];
+    }
+
+    /**
+     * Tests simple translating, where only translation is fetched
+     *
+     * @param string $template
+     * @param int    $languageId
+     * @param string $expected
+     *
+     * @dataProvider simpleTranslatingProvider
+     */
+    public function testSimpleTranslating($template, $languageId, $expected)
+    {
+        $this->setLanguage($languageId);
+        $this->assertEquals($expected, $this->getTemplate($template)->render([]));
+    }
+
+    /**
+     * Provides data to testTranslatingWithArguments
+     *
+     * @return array
+     */
+    public function withArgumentsProvider(): array
+    {
+        return [
+            ["{{ 'MANUFACTURER_S'|multi_lang('Opel') }}", 0, '| Hersteller: Opel'],
+            ["{{ 'MANUFACTURER_S'|multi_lang('Opel') }}", 1, 'Manufacturer: Opel'],
+            ["{{ 'INVITE_TO_SHOP'|multi_lang(['Admin', 'OXID Shop']) }}", 0, 'Eine Einladung von Admin OXID Shop zu besuchen.'],
+            ["{{ 'INVITE_TO_SHOP'|multi_lang(['Admin', 'OXID Shop']) }}", 1, 'An invitation from Admin to visit OXID Shop']
+        ];
+    }
+
+    /**
+     * Tests value translating when translating strings containing %s
+     *
+     * @param string $template
+     * @param int    $languageId
+     * @param string $expected
+     *
+     * @dataProvider withArgumentsProvider
+     */
+    public function testTranslatingWithArguments($template, $languageId, $expected)
+    {
+        $this->setLanguage($languageId);
+        $this->assertEquals($expected, $this->getTemplate($template)->render([]));
+    }
+
+    /**
+     * Provides data to testTranslateFrontend_isMissingTranslation
+     *
+     * @return array
+     */
+    public function missingTranslationProviderFrontend(): array
+    {
+        return [
+            [true, "{{ 'MY_MISING_TRANSLATION'|multi_lang }}", 'MY_MISING_TRANSLATION'],
+            [false, "{{ 'MY_MISING_TRANSLATION'|multi_lang }}", 'ERROR: Translation for MY_MISING_TRANSLATION not found!'],
+        ];
+    }
+
+    /**
+     * @param bool   $isProductiveMode
+     * @param string $template
+     * @param string $expected
+     *
+     * @dataProvider missingTranslationProviderFrontend
+     */
+    public function testTranslateFrontend_isMissingTranslation($isProductiveMode, $template, $expected)
+    {
+        $this->setAdminMode(false);
+        $this->setLanguage(1);
+
+        $oShop = $this->getConfig()->getActiveShop();
+        $oShop->oxshops__oxproductive = new Field($isProductiveMode);
+        $oShop->save();
+
+        $this->assertEquals($expected, $this->getTemplate($template)->render([]));
+    }
+
+    /**
+     * Provides data to testTranslateAdmin_isMissingTranslation
+     *
+     * @return array
+     */
+    public function missingTranslationProviderAdmin(): array
+    {
+        return [
+            ["{{ 'MY_MISING_TRANSLATION'|multi_lang }}", 'ERROR: Translation for MY_MISING_TRANSLATION not found!'],
+        ];
+    }
+
+    /**
+     * @param string $template
+     * @param string $expected
+     *
+     * @dataProvider missingTranslationProviderAdmin
+     */
+    public function testTranslateAdmin_isMissingTranslation($template, $expected)
+    {
+        $this->setLanguage(1);
+        $this->setAdminMode(true);
+
+        $this->assertEquals($expected, $this->getTemplate($template)->render([]));
+    }
+}
