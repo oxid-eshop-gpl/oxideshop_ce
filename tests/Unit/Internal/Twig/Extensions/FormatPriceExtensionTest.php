e19754e271862cedaff155e082df19e05633f972
Jędrzej Skoczek <j.skoczek@createit.pl>
2018-11-23 14:34:31 +0100

    OXDEV-1376 Convert oxprice to twig formatPrice

---
+++ tests/Unit/Internal/Twig/Extensions/FormatPriceExtensionTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\EshopCommunity\Tests\Unit\Internal\Twig\Extensions;
+
+use OxidEsales\EshopCommunity\Internal\Adapter\TemplateLogic\FormatPriceLogic;
+use OxidEsales\EshopCommunity\Internal\Twig\Extensions\FormatPriceExtension;
+use PHPUnit\Framework\TestCase;
+
+class FormatPriceExtensionTest extends TestCase
+{
+
+    /**
+     * @var FormatPriceExtension
+     */
+    private $formatPriceExtension;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $formatPriceLogic = new FormatPriceLogic();
+        $this->formatPriceExtension = new FormatPriceExtension($formatPriceLogic);
+    }
+
+    /**
+     * @covers \OxidEsales\EshopCommunity\Internal\Twig\Extensions\FormatPriceExtension::formatPrice
+     */
+    public function testFormatPrice()
+    {
+        $price = $this->formatPriceExtension->formatPrice(['price' => 100]);
+        $this->assertEquals('100,00 €', $price);
+    }
+}
