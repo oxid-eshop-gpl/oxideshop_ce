8f38e7ca2333a6e7fbb9d658208e4beb7b9395b2
Jędrzej Skoczek <j.skoczek@createit.pl>
2018-11-26 13:26:09 +0100

    OXDEV-1376 translated colon modifier to translateColon filter

---
+++ source/Internal/Twig/Extensions/Filters/TranslateColonExtension.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\EshopCommunity\Internal\Twig\Extensions\Filters;
+
+use Twig\Extension\AbstractExtension;
+
+/**
+ * Class TranslateColon
+ *
+ * @package OxidEsales\EshopCommunity\Internal\Twig\Extensions
+ */
+class TranslateColonExtension extends AbstractExtension
+{
+
+    /**
+     * @return array|\Twig_Filter[]
+     */
+    public function getFilters()
+    {
+        return [new \Twig_Filter('getTranslatedColon', [$this, 'getTranslatedColon'])];
+    }
+
+    /**
+     * Adds colon for selected language
+     *
+     * @param string $string
+     *
+     * @return mixed
+     */
+    public function getTranslatedColon($string)
+    {
+        $colon = \OxidEsales\Eshop\Core\Registry::getLang()->translateString('COLON');
+
+        return $string . $colon;
+    }
+}
