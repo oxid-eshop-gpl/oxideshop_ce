792dd249226db1c99bba848b1dc2be97870a4c8d
michael.koltan@oxid-esales.com <michael.koltan@oxid-esales.com>
2019-02-05 11:11:47 +0100

    Adds compiler pass to collect GraphQL query and mutation types

---
+++ source/Internal/Application/Utility/GraphQlTypePass.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\EshopCommunity\Internal\Application\Utility;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Class GraphQlTypePass
+ *
+ * This class configures the query/mutation type factories used
+ * to build the GraphQL schema. It collects all appropriately
+ * tagged queries / mutations and places them into the factories.
+ *
+ * @package OxidEsales\EshopCommunity\Internal\Application\Utility
+ */
+class GraphQlTypePass implements CompilerPassInterface
+{
+
+    protected $queryTypeFactoryId;
+    protected $queryTypeTag;
+    protected $mutationTypeFactoryId;
+    protected $mutationTypeTag;
+
+    /**
+     * GraphQlTypePass constructor.
+     *
+     * @param string $queryTypeFactoryId
+     * @param string $queryTypeTag
+     * @param string $mutationTypeFactoryId
+     * @param string $mutationTypeTag
+     */
+    public function __construct(
+        $queryTypeFactoryId = 'query_type_factory',
+        $queryTypeTag = 'graphql_query_type',
+        $mutationTypeFactoryId = 'mutation_type_factory',
+        $mutationTypeTag = 'graphql_mutation_type'
+    ) {
+        $this->queryTypeFactoryId = $queryTypeFactoryId;
+        $this->queryTypeTag = $queryTypeTag;
+        $this->mutationTypeFactoryId = $mutationTypeFactoryId;
+        $this->mutationTypeTag = $mutationTypeTag;
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @return null
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition($this->queryTypeFactoryId) && !$container->hasAlias($this->queryTypeFactoryId)) {
+            return;
+        }
+
+        if (!$container->hasDefinition($this->mutationTypeFactoryId) && !$container->hasAlias($this->mutationTypeFactoryId)) {
+            return;
+        }
+
+        $queryTypeFactoryDefinition = $container->findDefinition($this->queryTypeFactoryId);
+        $mutationTypeFactoryDefinition = $container->findDefinition($this->mutationTypeFactoryId);
+
+        foreach ($container->findTaggedServiceIds($this->queryTypeTag, true) as $id => $type) {
+            $queryTypeFactoryDefinition->addMethodCall('addSubType', array(new Reference($id)));
+        }
+        foreach ($container->findTaggedServiceIds($this->mutationTypeTag, true) as $id => $type) {
+            $mutationTypeFactoryDefinition->addMethodCall('addSubType', array(new Reference($id)));
+        }
+    }
+}
