78e2d6cb66f13544ca7c9d86ee41e3ba0cea5deb
vilmal <vilma.liorensaityte@gmail.com>
2021-02-25 15:26:58 +0100

    Make unit test green

--- tests/Integration/Internal/Transition/Smarty/Configuration/SmartySettingsDataProviderTest.php
+++ tests/Integration/Internal/Transition/Smarty/Configuration/SmartySettingsDataProviderTest.php
@@ -4,7 +4,7 @@
  * See LICENSE file for license details.
  */
 
-namespace OxidEsales\EshopCommunity\Tests\Unit\Internal\Transition\Smarty\Configuration;
+namespace OxidEsales\EshopCommunity\Tests\Integration\Internal\Transition\Smarty\Configuration;
 
 use OxidEsales\EshopCommunity\Core\Registry;
 use OxidEsales\EshopCommunity\Internal\Framework\Smarty\Configuration\SmartySettingsDataProvider;
