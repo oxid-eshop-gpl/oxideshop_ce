25068f03d4803385624304714f4fc9fcf8b2cc79
Anton Fedurtsya <anton@fedurtsya.com>
2021-12-30 15:33:36 +0200

    Language::translateString method returns string but not array
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- source/Core/Language.php
+++ source/Core/Language.php
@@ -427,7 +427,7 @@ class Language extends \OxidEsales\Eshop\Core\Base
      * @param int    $iLang              optional language number
      * @param bool   $blAdminMode        on special case you can force mode, to load language constant from admin/shops language file
      *
-     * @return string|array
+     * @return string
      */
     public function translateString($sStringToTranslate, $iLang = null, $blAdminMode = null)
     {
