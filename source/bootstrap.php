b67c60997d566106c1f632d8a96985347a40de49
vilmal <vilma.liorensaityte@gmail.com>
2022-08-09 16:34:51 +0200

    Remove legacy tests

--- source/bootstrap.php
+++ source/bootstrap.php
@@ -5,6 +5,9 @@
  * See LICENSE file for license details.
  */
 
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+ini_set('display_errors', '0');
+
 define('INSTALLATION_ROOT_PATH', dirname(__DIR__));
 define('OX_BASE_PATH', INSTALLATION_ROOT_PATH . DIRECTORY_SEPARATOR . 'source' . DIRECTORY_SEPARATOR);
 define('OX_LOG_FILE', OX_BASE_PATH . 'log' . DIRECTORY_SEPARATOR . 'oxideshop.log');
